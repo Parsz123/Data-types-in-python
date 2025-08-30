98003626f559601a222c5d815718389051c7fbd6
Parsz123 <hetaldr@yahoo.com>
2025-08-30 10:36:51 +0100

    Activity 2

---
+++ Typecasting variables.py
@@ -0,0 +1,20 @@
+age = 28
+weight = 35.8346
+name = "Parshva"
+Penguin = True
+
+print("The data type of age:", type(age))
+print("The data type of weight:", type(weight))
+print("The data type of name:", type(name))
+print("The data type of Penguin:", type(Penguin))
+
+age = str(age)
+weight = int(weight)
+name = bool(name)
+Penguin = float(Penguin)
+
+
+print("The data type of age:", type(age))
+print("The data type of weight:", type(weight))
+print("The data type of name:", type(name))
+print("The data type of Penguin:", type(Penguin))
